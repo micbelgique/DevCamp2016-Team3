a4de48e4207aefd12840470acf6c1047dcab24c2
sguerin <sguerin>
2016-08-05 14:43:08 +0200

    mission service added

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { MissionService } from './services/mission-service';
 import { ROUTER_DIRECTIVES }  from '@angular/router';
 
 // decorator function that allows us to associate metadata with the component class
 @Component({
   directives: [
     ROUTER_DIRECTIVES
+  ],
+  providers: [
+    HTTP_PROVIDERS,
+    MissionService
   ],
   selector: 'my-app',
   templateUrl: 'app/app.component.html'
