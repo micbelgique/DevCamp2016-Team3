47def5f97c2b3a0e32ac3b7c0bce323fdcf06ae7
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-05 14:30:08 +0200

    Updated mission schema

--- backend/models/schemas/mission.js
+++ backend/models/schemas/mission.js
@@ -9,6 +9,22 @@ const schema = new mongoose.Schema({
   description: {
     required: true,
     type: String
+  },
+  place: {
+    required: true,
+    type: String
+  },
+  sponsor: {
+    required: true,
+    type: String
+  },
+  rating: {
+    required: true,
+    type: Number
+  },
+  difficulty: {
+    required: true,
+    type: Number
   }
 }, {
   timestamps: true,
