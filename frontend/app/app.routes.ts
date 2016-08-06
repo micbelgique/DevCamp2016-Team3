5ea5c36cc6bc13ffa350a90bcb4efeb809565704
sguerin <sguerin>
2016-08-06 13:52:20 +0200

    empty exploration checkpoint component added

--- frontend/app/app.routes.ts
+++ frontend/app/app.routes.ts
@@ -2,6 +2,7 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { MissionsListComponent } from './missions-list.component';
 import { MissionDetailsComponent } from './mission-details.component';
 import { ExplorationComponent } from './exploration.component';
+import { ExplorationCheckpointComponent } from './exploration-checkpoint.component';
 
 const routes: RouterConfig = [
     {
@@ -10,17 +11,21 @@ const routes: RouterConfig = [
         pathMatch: 'full'
     },
     {
+        path: 'explorations/:slug',
+        component: ExplorationComponent
+    },
+    {
+        path: 'explorations/:slugex/checkpoints/:slugcp',
+        component: ExplorationCheckpointComponent
+    },
+    {
         path: 'missions',
         component: MissionsListComponent
     },
     {
         path: 'missions/:slug',
         component: MissionDetailsComponent
-    },
-    {
-        path: 'explorations/:slug',
-        component: ExplorationComponent
-    },
+    }
 ];
 
 export const appRouterProviders = [
--- frontend/app/exploration.component.ts
+++ frontend/app/exploration.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'exploration',
     templateUrl: 'app/exploration.component.html'
 })
 export class ExplorationComponent implements OnInit {
-    constructor() { }
+
+    constructor(private _router: Router) { }
+
+    goToCheckpoint() {
+        this._router.navigate(['/explorations/test-test/checkpoints/test-test']);
+    }
 
     ngOnInit() { }
 
