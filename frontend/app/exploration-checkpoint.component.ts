fbca07992f43f4f7f69b98db693a357be75476c5
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-06 22:23:36 +0200

    Added angular2 file uploader

--- frontend/app/exploration-checkpoint.component.ts
+++ frontend/app/exploration-checkpoint.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FILE_UPLOAD_DIRECTIVES, FileUploader } from 'ng2-file-upload';
+
+import { AppSettings } from './app.settings';
 
 @Component({
     selector: 'exploration-checkpoint',
-    templateUrl: 'app/exploration-checkpoint.component.html'
+    templateUrl: 'app/exploration-checkpoint.component.html',
+    directives: [FILE_UPLOAD_DIRECTIVES]
 })
 export class ExplorationCheckpointComponent implements OnInit {
-    constructor() { }
+    public uploader:FileUploader
+
+    constructor (appSettings: AppSettings) {
+        this.uploader = new FileUploader({
+            url: `${appSettings.baseUrl}/explorations/xxx/checkpoints/yyy` // TODO
+        });
+        this.uploader.onBuildItemForm = (item, form) => {
+            form.append("checkpoint", "yyy"); // TODO
+        };
+    }
 
     ngOnInit() { }
-
 }
