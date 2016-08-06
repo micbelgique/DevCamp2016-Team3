7a6f93a442be39b49730e3ccfa1376d49f367647
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-06 11:49:24 +0200

    Updated mission model with checkpoints

--- frontend/app/models/mission.ts
+++ frontend/app/models/mission.ts
@@ -1,9 +1,10 @@
+import { Checkpoint } from './checkpoint';
+
 export class Mission {
     description: string;
     id: number;
     illustration: string;
     name: string;
     slug: string;
-
-    constructor() {}
+    checkpoints: Array<Checkpoint>;
 }
