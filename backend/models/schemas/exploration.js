0ec858635b21a126077571de6ffe90e97655d918
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-06 12:22:20 +0200

    Added completed checkpoints to exploration

--- backend/models/schemas/exploration.js
+++ backend/models/schemas/exploration.js
@@ -10,7 +10,8 @@ const schema = new mongoose.Schema({
   name: {
     required: true,
     type: String
-  }
+  },
+  completed: [ { type: mongoose.Schema.Types.ObjectId } ]
 }, {
   timestamps: true,
   toObject: {
