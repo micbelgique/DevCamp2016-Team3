ecfc43839f1e2d9d23499b601a44b1ff1caee95c
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-06 11:58:23 +0200

    Fixed mission list category

--- frontend/app/mission-list-item.component.ts
+++ frontend/app/mission-list-item.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Mission } from './models/mission';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { CategoryPipe } from './pipes/category.pipe';
 
 @Component({
     directives: [
         ROUTER_DIRECTIVES
     ],
     selector: 'mission-list-item',
-    templateUrl: 'app/mission-list-item.component.html'
+    templateUrl: 'app/mission-list-item.component.html',
+    pipes: [CategoryPipe]
 })
 export class MissionListItemComponent implements OnInit {
 
