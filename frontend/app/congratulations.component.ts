71eacf637f516f4569e1b0be57b3db7ae705d143
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-07 11:27:19 +0200

    Updated congratulations to get data from service

--- frontend/app/congratulations.component.ts
+++ frontend/app/congratulations.component.ts
@@ -1,12 +1,38 @@
+import { ActivatedRoute, ROUTER_DIRECTIVES, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Mission } from './models/mission';
+import { Observable } from 'rxjs/Rx';
+import { MissionService } from './services/missions.service';
+import { CategoryPipe } from './pipes/category.pipe';
 
 @Component({
     selector: 'congratulations',
-    templateUrl: 'app/congratulations.component.html'
+    templateUrl: 'app/congratulations.component.html',
+    pipes: [CategoryPipe]
 })
 export class CongratulationsComponent implements OnInit {
-    constructor() { }
 
-    ngOnInit() { }
+    private errorMessage: string;
+    private mission: Mission;
+    private sub: any;
 
+    constructor(private missionService: MissionService, private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        this.sub = this.route.params.subscribe(params => {
+            if (params['slug'] !== undefined) {
+                this.missionService.getMissionBySlug(params['slug'])
+                    .subscribe(
+                    m => {
+                        this.mission = m;
+                    },
+                    err => this.errorMessage = err
+                    );
+            }
+        });
+     }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
 }
