f1878d167ce317c1a4c0625f38380469c2c3a6d3
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-09 10:11:09 +0200

    Added catch-all route to support SPA router

--- frontend/app.js
+++ frontend/app.js
@@ -4,7 +4,10 @@ var logger = require("morgan");
 var app = express();
 
 app.use(logger("dev"));
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
+app.all("*", function (req, res) {
+  res.sendFile("index.html", { root: __dirname });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
